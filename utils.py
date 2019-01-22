473dc622737222c9122c24c8b3c37e6c603a1e67
juliiami <45482942+juliiami@users.noreply.github.com>
2019-01-22 15:33:25 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -24,8 +24,8 @@ SIMPLE_FEATURE_COLUMNS = ['ncl[0]', 'ncl[1]', 'ncl[2]', 'ncl[3]', 'avg_cs[0]',
 
 TRAIN_COLUMNS = ["label", "weight"]
 
-FOI_COLUMNS = ["FOI_hits_X", "FOI_hits_Y", "FOI_hits_T",
-               "FOI_hits_Z", "FOI_hits_DX", "FOI_hits_DY", "FOI_hits_S"]
+FOI_COLUMNS = ["FOI_hits_X", "FOI_hits_Y", "FOI_hits_Z",
+               "FOI_hits_DX", "FOI_hits_DY","FOI_hits_DZ",  "FOI_hits_T",  "FOI_hits_DT", "FOI_hits_S"]
 
 ID_COLUMN = "id"
 
@@ -42,17 +42,17 @@ EMPTY_FILLER = 1000
 # which is not guranteed
 def load_train_hdf(path):
     return pd.concat([
-        pd.read_hdf(os.path.join(path, "train_part_%i.hdf" % i))
+        pd.read_hdf(os.path.join(path, "train_part_%i_v2.hdf" % i))
         for i in (1, 2)], axis=0, ignore_index=True)
 
 
 def load_data_csv(path, feature_columns):
     train = pd.concat([
-        pd.read_csv(os.path.join(path, "train_part_%i.csv.gz" % i),
+        pd.read_csv(os.path.join(path, "train_part_%i_v2.csv.gz" % i),
                     usecols= [ID_COLUMN] + feature_columns + TRAIN_COLUMNS,
                     index_col=ID_COLUMN)
         for i in (1, 2)], axis=0, ignore_index=True)
-    test = pd.read_csv(os.path.join(path, "test_public.csv.gz"),
+    test = pd.read_csv(os.path.join(path, "test_public_v2.csv.gz"),
                        usecols=[ID_COLUMN] + feature_columns, index_col=ID_COLUMN)
     return train, test
 
@@ -61,10 +61,20 @@ def parse_array(line, dtype=np.float32):
     return np.fromstring(line[1:-1], sep=" ", dtype=dtype)
 
 
+def load_full_train_csv(path):
+    converters = dict(zip(FOI_COLUMNS, repeat(parse_array)))
+    types = dict(zip(SIMPLE_FEATURE_COLUMNS, repeat(np.float32)))
+    train = pd.concat([
+        pd.read_csv(os.path.join(path, "train_part_%i_v2.csv.gz" % i),
+                    usecols= [ID_COLUMN]+SIMPLE_FEATURE_COLUMNS+FOI_COLUMNS+ TRAIN_COLUMNS,
+                    index_col=ID_COLUMN)
+        for i in (1, 2)], axis=0, ignore_index=True)
+    return train
+
 def load_full_test_csv(path):
     converters = dict(zip(FOI_COLUMNS, repeat(parse_array)))
     types = dict(zip(SIMPLE_FEATURE_COLUMNS, repeat(np.float32)))
-    test = pd.read_csv(os.path.join(path, "test_public.csv.gz"),
+    test = pd.read_csv(os.path.join(path, "test_public_v2.csv.gz"),
                        index_col="id", converters=converters,
                        dtype=types,
                        usecols=[ID_COLUMN]+SIMPLE_FEATURE_COLUMNS+FOI_COLUMNS)
